0a1a54b1ad5f9ecd2b6c40992c2539782b30321c
Bushra AlQahtani <89680878+bushra-alqahtani@users.noreply.github.com>
2022-06-05 15:57:24 +0300

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,40 @@
+
+#1
+from xml.etree.ElementTree import tostring
+
+
+print("hello world")
+
+#2a
+name="Bushra"
+print("hello",name)
+
+#2b
+name="Bushra"
+print("hello"+" "+name)
+
+#3a
+num=23
+print("hello",num)
+#3b
+num="23"
+print("hello"+ num )
+
+#4a
+food1="pasta"
+food2="sushi"
+
+print("I love to eat {} and {}".format(food1 , food2))
+
+
+#4b
+food1="pasta"
+food2="sushi"
+
+print(f"I love to eat {food1} and {food2}")
+
+#NINJA BONUS
+food1="%s"%"pasta "
+food2=" %s"%"sushi"
+
+print("I love to eat %s and %s" %(food1,food2))
